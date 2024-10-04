1aa150177ef0c11c27c091913228709915d418e6
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-03 22:01:50 -0300

    feat: dogs conversation

--- 02 - Programação Orientada a Objetos/03 - Construtores e destrutores/01_construtores_destrutores.py
+++ 02 - Programação Orientada a Objetos/03 - Construtores e destrutores/01_construtores_destrutores.py
@@ -1,31 +1,53 @@
 class Cachorro:
     def __init__(self, nome, cor, acordado=True):
-        print("Inicializando a classe...")
+        print("\nInicializando a classe...")
         self.nome = nome
         self.cor = cor
         self.acordado = acordado
+        print("Classe iniciada" + str(vars(self)))
+
 
     def __del__(self):
-        print("Removendo a instância da classe.")
+        print(f"Removendo a instância da classe: {self.nome}")
+        print(f"{self.nome} say: auuuuuuuuuuuuuuu\n")
 
     def falar(self):
-        print("auau")
+        if(self.acordado):
+            print(self.nome + " say: Au Au")
+        else:
+            print(self.nome + ":  Zzzzzz  Zzzzzz  Zzzzzz")
 
 
 def criar_cachorro():
     c = Cachorro("Zeus", "Branco e preto", False)
-    print(c.nome)
+    return c
+    # print(c.nome)
 
 
 c = Cachorro("Chappie", "amarelo")
 c.falar()
-
-print("Ola mundo")
-
 del c
 
-print("Ola mundo")
-print("Ola mundo")
-print("Ola mundo")
+c = Cachorro("Dog", "gray", False)
+print(f"{c.nome}!!!")
+c.falar() 
 
-# criar_cachorro()
+if(c.acordado == False):
+    print(f"{c.nome} wake up!")
+    c.acordado = True
+c.falar() 
+
+c.acordado = True
+print(f"{c.nome}!!!")
+c.falar()
+
+
+z = criar_cachorro()
+z.falar() 
+if(z.acordado == False):
+    print(f"{z.nome} wake up!")
+    z.acordado = True
+print(f"{z.nome}!!!")
+
+z.falar()
+c.falar()
