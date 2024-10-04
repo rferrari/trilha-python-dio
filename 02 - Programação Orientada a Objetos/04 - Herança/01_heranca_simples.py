1310b39cdae4d0870ab110d6bde4803d10683c2b
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-03 22:24:51 -0300

    feat: loading truck

--- 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
@@ -1,3 +1,5 @@
+import time
+
 class Veiculo:
     def __init__(self, cor, placa, numero_rodas):
         self.cor = cor
@@ -5,7 +7,7 @@ class Veiculo:
         self.numero_rodas = numero_rodas
 
     def ligar_motor(self):
-        print("Ligando o motor")
+        print(self.placa + ": Ligando o motor")
 
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
@@ -22,16 +24,40 @@ class Carro(Veiculo):
 class Caminhao(Veiculo):
     def __init__(self, cor, placa, numero_rodas, carregado):
         super().__init__(cor, placa, numero_rodas)
-        self.carregado = carregado
+        self.peso = 0
+        self.carregado = False
 
     def esta_carregado(self):
-        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+        return self.carregado
+    
+    def carrega(self, peso):
+        self.peso += peso
+        time.sleep(0.35)
+        print("||" * (100 - (100 - self.peso // 10)))
+        return self.carregado
+    
+    def peso_carga(self):
+        return self.peso
 
 
-moto = Motocicleta("preta", "abc-1234", 2)
-carro = Carro("branco", "xde-0098", 4)
-caminhao = Caminhao("roxo", "gfd-8712", 8, True)
+moto = Motocicleta("preta", "moto-1234", 2)
+carro = Carro("branco", "carro-0098", 4)
+caminhao = Caminhao("roxo", "truck-8712", 8, True)
 
 print(moto)
 print(carro)
 print(caminhao)
+
+print("Caminhao esta carregado? " + str(caminhao.esta_carregado()))
+while (not caminhao.carregado):
+    if(caminhao.peso_carga() == 100):
+        caminhao.carregado = True
+    if(caminhao.carregado):
+        print("Sim estou carregado")
+    else:
+        caminhao.carrega(10)
+    print(caminhao.peso_carga())
+
+moto.ligar_motor()
+carro.ligar_motor()
+caminhao.ligar_motor()
