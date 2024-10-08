9ecfde0c901c66d9ee9f56746252910f7a43a78d
Ricardo Ferrari <rferrariwd@gmail.com>
2024-10-07 23:02:20 -0300

    feat: properties

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
@@ -1,7 +1,13 @@
 class Pessoa:
-    def __init__(self, nome, ano_nascimento):
-        self.nome = nome
+    def __init__(self, nome, sobrenome, nomecompleto = "", ano_nascimento = -1):
+        self._nome = nome
+        self._sobrenome = sobrenome
+#        self._nomecomleto = nomecomleto if nomecomleto != "" else nomecomleto = (self._nome + " " + self._sobrenome)
+        self._nomecomleto = nomecompleto if nomecompleto != "" else self._nome + " " + self._sobrenome 
         self._ano_nascimento = ano_nascimento
+
+    def nomecompleto(self):
+        return self._nomecomleto
 
     @property
     def idade(self):
@@ -9,5 +15,7 @@ class Pessoa:
         return _ano_atual - self._ano_nascimento
 
 
-pessoa = Pessoa("Guilherme", 1994)
-print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
+pessoa = Pessoa("Guilherme","Ramos", "", 1994)
+print(pessoa.nomecompleto())
+
+# print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
